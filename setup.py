c6dc82c6f3b2b1d9e2b1dff53ec98bdfa05de0b9
kubotaku1119 <kubotaku1119@gmail.com>
2019-07-19 10:56:52 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='mui_ui',
-    version='0.1.2',
+    version='0.2.0',
     description='UI Library for mui',
     long_description=readme,
     author='Takuya Kubota',
